fa433982878fddffe8dfa1f8d092968fdb2f4d59
Takayuki Kamiyama <karuma.reason@gmail.com>
2021-03-19 17:02:47 +0900

    Update, code format.

--- rplugin/python3/deoplete/sources/go_straight.py
+++ rplugin/python3/deoplete/sources/go_straight.py
@@ -8,52 +8,12 @@ import dask.dataframe as dd
 import pandas as pd
 from deoplete.source.base import Base
 
-# ------------------------------- KEYWORD -------------------------------------------------------------------------
-
-
-home = expanduser("~")
-
-d1 = os.path.expanduser("~/.vim/.cache/dein/repos/github.com/takkii/go_straight/dict/")
-d2 = os.path.expanduser("~/.vim/repos/github.com/takkii/go_straight/dict/")
-d3 = os.path.expanduser("~/.cache/dein/repos/github.com/takkii/go_straight/dict/")
-d4 = os.path.expanduser("~/.config/nvim/.cache/dein/repos/github.com/takkii/go_straight/dict/")
-d5 = os.path.expanduser("~/.config/nvim/repos/github.com/takkii/go_straight/dict/")
-
-if os.path.isdir(d1):
-    ruby_method = open(os.path.expanduser(
-        "~/.vim/.cache/dein/repos/github.com/takkii/go_straight/dict/ruby_dict"))
-elif os.path.isdir(d2):
-    ruby_method = open(os.path.expanduser(
-        "~/.vim/repos/github.com/takkii/go_straight/dict/ruby_dict"))
-elif os.path.isdir(d3):
-    ruby_method = open(os.path.expanduser(
-        "~/.cache/dein/repos/github.com/takkii/go_straight/dict/ruby_dict"))
-elif os.path.isdir(d4):
-    ruby_method = open(os.path.expanduser(
-        "~/.config/nvim/.cache/dein/repos/github.com/takkii/go_straight/dict/ruby_dict"))
-elif os.path.isdir(d5):
-    ruby_method = open(os.path.expanduser(
-        "~/.config/nvim/repos/github.com/takkii/go_straight/dict/ruby_dict"))
-else:
-    print("Please, Check the path of go_straight.")
-
-index_ruby = list(ruby_method.readlines())
-Seri = pd.Series(index_ruby)
-sort_ruby = Seri.sort_index()
-ddf = dd.from_pandas(data=sort_ruby, npartitions=multiprocessing.cpu_count())
-data = ddf.to_dask_array(lengths=True)
-data = ddf.compute()
-data_ruby = list(map(lambda s: s.rstrip(), data))
-ruby_method.close()
-
-
-# ------------------------------- KEYWORD -------------------------------------------------------------------------
 
 class Source(Base):
     def __init__(self, vim):
         super().__init__(vim)
         self.name = 'go_straight'
-        self.filetypes = ['ruby']
+        self.filetypes = ['ruby, erb, html']
         self.mark = '[Go_Straight!]'
         rubymatch = [r'\.[a-zA-Z0-9_?!]*|[a-zA-Z]\w*::\w*']
         regexmatch = [r'[<a-zA-Z(?: .+?)?>.*?<\/a-zA-Z>]']
@@ -66,8 +26,43 @@ class Source(Base):
 
     def gather_candidates(self, context):
         try:
+            home = expanduser("~")
+            d1 = os.path.expanduser("~/.vim/.cache/dein/repos/github.com/takkii/go_straight/dict/")
+            d2 = os.path.expanduser("~/.vim/repos/github.com/takkii/go_straight/dict/")
+            d3 = os.path.expanduser("~/.cache/dein/repos/github.com/takkii/go_straight/dict/")
+            d4 = os.path.expanduser("~/.config/nvim/.cache/dein/repos/github.com/takkii/go_straight/dict/")
+            d5 = os.path.expanduser("~/.config/nvim/repos/github.com/takkii/go_straight/dict/")
+
+            if os.path.isdir(d1):
+                ruby_method = open(os.path.expanduser(
+                    "~/.vim/.cache/dein/repos/github.com/takkii/go_straight/dict/ruby_dict"))
+            elif os.path.isdir(d2):
+                ruby_method = open(os.path.expanduser(
+                    "~/.vim/repos/github.com/takkii/go_straight/dict/ruby_dict"))
+            elif os.path.isdir(d3):
+                ruby_method = open(os.path.expanduser(
+                    "~/.cache/dein/repos/github.com/takkii/go_straight/dict/ruby_dict"))
+            elif os.path.isdir(d4):
+                ruby_method = open(os.path.expanduser(
+                    "~/.config/nvim/.cache/dein/repos/github.com/takkii/go_straight/dict/ruby_dict"))
+            elif os.path.isdir(d5):
+                ruby_method = open(os.path.expanduser(
+                    "~/.config/nvim/repos/github.com/takkii/go_straight/dict/ruby_dict"))
+            else:
+                print("Please, Check the path of go_straight.")
+
+            index_ruby = list(ruby_method.readlines())
+            Seri = pd.Series(index_ruby)
+            sort_ruby = Seri.sort_index()
+            ddf = dd.from_pandas(data=sort_ruby, npartitions=multiprocessing.cpu_count())
+            data = ddf.to_dask_array(lengths=True)
+            data = ddf.compute()
+            data_ruby = list(map(lambda s: s.rstrip(), data))
+            ruby_method.close()
+
             dic = data_ruby
             dic_sort = sorted(dic, key=lambda dic: dic[0])
             return dic_sort
+
         except Exception:
             traceback.print_exc()
