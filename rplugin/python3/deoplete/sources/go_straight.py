52b0e6e5ce9652163b72fc411932c0560a32b556
Takayuki Kamiyama <karuma.reason@gmail.com>
2021-03-25 14:10:03 +0900

    Revert.

--- rplugin/python3/deoplete/sources/go_straight.py
+++ rplugin/python3/deoplete/sources/go_straight.py
@@ -15,9 +15,9 @@ class Source(Base):
         self.name = 'go_straight'
         self.filetypes = ['ruby']
         self.mark = '[Go_Straight!]'
-        regexmatch = [r'[<a-zA-Z(?: .+?)?>.*?<\/a-zA-Z>]']
-        pathmatch = [r'[\/.a-zA-Z0-9_?!.\/]*']
-        self.input_pattern = '|'.join(pathmatch + regexmatch)
+        ruby_match = [r'\.[a-zA-Z0-9_?!]*|[a-zA-Z]\w*::\w*']
+        regex_match = [r'[<a-zA-Z(?: .+?)?>.*?<\/a-zA-Z>]']
+        self.input_pattern = '|'.join(ruby_match + regex_match)
         self.rank = 500
 
     def get_complete_position(self, context):
