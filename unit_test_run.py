89bb439ff962cd6638c166a3c52564a956fefed6
Takayuki Kamiyama <karuma.reason@gmail.com>
2023-10-23 11:57:14 +0900

    Update.

--- unit_test_run.py
+++ unit_test_run.py
@@ -3,6 +3,7 @@ import unittest
 from typing import Optional
 
 
+# Python Version Check.
 class Checker:
     def version(self):
         major: Optional[int] = sys.version_info[0]
@@ -10,6 +11,7 @@ class Checker:
         print("Version : " + str(major) + "." + str(minor) + " : ", id(self))
 
 
+# Unit Test.
 class VersionTest(unittest.TestCase):
     @classmethod
     def setupClass(cls):
@@ -19,6 +21,7 @@ class VersionTest(unittest.TestCase):
     def tearDownClass(cls):
         print("tearDownClass")
 
+    # Create Instance.
     def setUp(self):
         self.checker = Checker()
         print("setUp", id(self))
@@ -27,18 +30,20 @@ class VersionTest(unittest.TestCase):
         # self.checker.version()
         print("tearDown", id(self))
 
+    # Python Version 3.x
     def test_check_major(self):
         py_major: Optional[int] = sys.version_info[0]
         major_calc = int(py_major)
         set_major = int(4)
-        print("test_check", id(self))
+        print("test_check_major", id(self))
         self.assertLess(major_calc, set_major)
 
+    # Python Version x.11 or x.10
     def test_check_minor(self):
         py_minor: Optional[int] = sys.version_info[1]
         minor_calc = int(py_minor)
         set_minor = int(0)
-        print("test_check", id(self))
+        print("test_check_minor", id(self))
         self.assertGreater(minor_calc, set_minor)
 
 
