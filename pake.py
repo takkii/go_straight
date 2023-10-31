6d5e1e3d5456075322c76216a9fe3ac514199242
Takayuki Kamiyama <karuma.reason@gmail.com>
2023-10-31 13:35:38 +0900

    Update.

--- pake.py
+++ pake.py
@@ -1,3 +1,15 @@
-with open('./unit/test_run.py') as ut:
+# Run, unit/xunit.py
+
+print('----------------------------------------------------------------------')
+
+with open('./unit/xunit.py') as xut:
+    xcmd = xut.read()
+    exec(xcmd)
+
+print('----------------------------------------------------------------------')
+
+# Run, unit/unit.py
+
+with open('./unit/unit.py') as ut:
     cmd = ut.read()
     exec(cmd)
