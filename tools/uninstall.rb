f6ba0592a952e5ebb9407da4af7abb8204f72723
Takayuki Kamiyama <karuma.reason@gmail.com>
2023-11-22 19:01:16 +0900

    Update.

---
+++ tools/uninstall.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'open3'
+require 'fileutils'
+
+# Installer runner.
+class UnInstallerRunner
+  # default encoding utf-8, change encode here.
+  def self.encoding_style
+    Encoding.default_internal = 'UTF-8'
+    Encoding.default_external = 'UTF-8'
+  end
+
+  def self.run
+    encoding_style
+    FileUtils.rm_rf(File.expand_path('~/config'))
+    stdout_rq, _stderr_rq, _status_rq = Open3.capture3('pip3 uninstall -y -r requirements.txt')
+    stdout_rq
+  end
+end
+
+begin
+  UnInstallerRunner.run
+rescue StandardError => e
+  puts e.backtrace
+ensure
+  GC.compact
+end
+
+__END__
